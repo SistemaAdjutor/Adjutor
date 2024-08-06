62751db3b7397fd190678c737f5f5796317c27bf
Sistema Adjutor <adjutor@novisistemas.com.br>
2024-08-06 12:41:39 -0300

    versão 595

--- infosaga/InicioDB.pas
+++ infosaga/InicioDB.pas
@@ -3042,7 +3042,7 @@ procedure TDBInicio.LerVersao;
         Close;
       end;
     end;
-    vlVersao.patch := 4;
+    vlVersao.patch := 0;
     vlVersao.Issues := '';
 
     r := TIdIPWatch.Create( nil );
