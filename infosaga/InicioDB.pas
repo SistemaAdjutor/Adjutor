95eb842ade61c8a85fb27eaf3943bc3e67ae2734
Sistema Adjutor <adjutor@novisistemas.com.br>
2025-03-17 10:07:52 -0300

    versão 602 patch 1

--- infosaga/InicioDB.pas
+++ infosaga/InicioDB.pas
@@ -3042,8 +3042,8 @@ procedure TDBInicio.LerVersao;
         Close;
       end;
     end;
-    vlVersao.patch := 0;
-    vlVersao.Issues := '2022';
+    vlVersao.patch := 1;
+    vlVersao.Issues := '';
 
     r := TIdIPWatch.Create( nil );
     try
