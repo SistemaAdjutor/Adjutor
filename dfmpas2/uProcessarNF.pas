82f82662c8b2d34e4adf07a3901ca7e3620db27a
Sistema Adjutor <adjutor@novisistemas.com.br>
2026-01-09 15:01:25 -0300

    2133-2137

--- dfmpas2/uProcessarNF.pas
+++ dfmpas2/uProcessarNF.pas
@@ -2975,10 +2975,10 @@ begin
    and (dbInicio.BuscaUmDadoSqlAsString('SELECT OPE_MOSTRA_IBS_CBS FROM OPE0000 WHERE OPE_CODIGO = ' + QuotedStr(qNota.FieldByName('OPE_CODIGO').AsString)) = 'S')
   then
   begin
+    AliqUF := BuscaUmDadoSQLAsFloat('SELECT PMT_IBS_ALIQUOTA_ESTADUAL FROM PRMT0001 WHERE EMP_CODIGO = ' + QuotedStr(dbInicio.EMP_CODIGO));
     if (dbInicio.GetParametroSistema('PMT_SOMA_IBS_UF_BASE_CALCULO') = 'S') then
     begin
-      AliqUF := BuscaUmDadoSQLAsFloat('SELECT PMT_IBS_ALIQUOTA_ESTADUAL FROM PRMT0001 WHERE EMP_CODIGO = ' + QuotedStr(dbInicio.EMP_CODIGO));
-      notaf.NFe.Total.IBSCBSTot.vBCIBSCBS := qNota.FieldByName('NF_TOT_PROD').AsFloat; //  + ((qNota.FieldByName('NF_TOT_PROD').AsFloat / 100) * AliqUF  );
+      notaf.NFe.Total.IBSCBSTot.vBCIBSCBS := qNota.FieldByName('NF_TOT_PROD').AsFloat + ((qNota.FieldByName('NF_TOT_PROD').AsFloat / 100) * AliqUF  );
     end
     else
       notaf.NFe.Total.IBSCBSTot.vBCIBSCBS := qNota.FieldByName('NF_TOT_PROD').AsFloat;
@@ -2987,6 +2987,7 @@ begin
 
     AliqMun := BuscaUmDadoSQLAsFloat('SELECT PMT_IBS_ALIQUOTA_MUNICIPAL FROM PRMT0001 WHERE EMP_CODIGO = ' + QuotedStr(dbInicio.EMP_CODIGO));
     notaf.NFe.Total.IBSCBSTot.gIBS.gIBSMunTot.vIBSMun := ((qNota.FieldByName('NF_TOT_PROD').AsFloat / 100) * AliqMun  );
+    notaf.NFe.Total.IBSCBSTot.gIBS.vIBS := ((qNota.FieldByName('NF_TOT_PROD').AsFloat / 100) * AliqUF  );
 
     AliqCBS := BuscaUmDadoSQLAsFloat('SELECT PMT_CBS_ALIQUOTA FROM PRMT0001 WHERE EMP_CODIGO = ' + QuotedStr(dbInicio.EMP_CODIGO));
     notaf.NFe.Total.IBSCBSTot.gCBS.vCBS := ((qNota.FieldByName('NF_TOT_PROD').AsFloat / 100) * AliqCBS  );
