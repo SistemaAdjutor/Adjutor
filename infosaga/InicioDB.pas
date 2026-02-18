18c1afd83a03f3a5463d9980beb1013e5040ed93
Sistema Adjutor <adjutor@novisistemas.com.br>
2026-02-18 13:08:28 -0300

    Versão 611 patch 3

--- infosaga/InicioDB.pas
+++ infosaga/InicioDB.pas
@@ -3042,7 +3042,7 @@ procedure TDBInicio.LerVersao;
         Close;
       end;
     end;
-    vlVersao.patch := 2;
+    vlVersao.patch := 3;
     vlVersao.Issues := '';
 
     r := TIdIPWatch.Create( nil );
